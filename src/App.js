916252aaf5c7e8e01b48ede25113d35c3e1525f9
Kajal <kajana@amdocs.com>
2024-03-18 00:14:16 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import "./styles.css";
+import { useState } from "react";
+
+export default function App() {
+  const [first, setFirst] = useState("");
+  const [last, setLast] = useState("");
+  const [show, setShow] = useState(false);
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("hi");
+    //setData();
+    setShow(true);
+  };
+  return (
+    <div className="App">
+      <form onSubmit={handleSubmit}>
+        <label>
+          First Name:
+          <input
+            type="text"
+            required
+            onChange={(e) => setFirst(e.target.value)}
+          />
+        </label>
+        <label>
+          Last Name:
+          <input
+            type="text"
+            required
+            onChange={(e) => setLast(e.target.value)}
+          />
+        </label>
+        <button>Submit</button>
+      </form>
+      {show ? (
+        <div>
+          Full Name:{" "}
+          <span>
+            {first} {last}
+          </span>
+        </div>
+      ) : (
+        ""
+      )}
+    </div>
+  );
+}
